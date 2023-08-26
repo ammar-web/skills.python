8eced8c77f8ade0f4762c0c76d45e0deaf7bc055
Coding Knight <141774089+ammar-web@users.noreply.github.com>
2023-08-26 09:31:51 +0530

    Add files via upload

---
+++ if,elif and else functions.py
@@ -0,0 +1,24 @@
+x=(input("enter a name:"))
+
+
+if len(x)<3:
+    print("name cannot be less than 3 characters!")
+
+
+elif len(x)>50:
+    print("name must be of maximum fifty characters!")
+
+else:
+    print("name looks good!")
+
+
+y=int(input("enter your weight:"))
+z=(input("lbs or kg:"))
+
+if z in "lbs":
+    print(f"Your converted weight: {y*0.45}")
+
+elif z in "kg":
+    print(f" Your converterd weight: {y/0.45}")
+else:
+    print("error!")
