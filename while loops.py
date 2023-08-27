20e06d2c5d7a08211fdbe400fd0e5c4b505373d8
Coding Knight <141774089+ammar-web@users.noreply.github.com>
2023-08-27 19:14:41 +0530

    Update while loops.py

--- while loops.py
+++ while loops.py
@@ -8,10 +8,19 @@ guess_count=0
 guess_limit=3
 
 while guess_count < guess_limit:
+    
     guess=int(input("guess:"))
+    
     guess_count+=1
 
-if guess==secret_number:
+    
+    if guess==secret_number:
+        
         print("you won")
+        
+        break
 
 else:print("you loose")
+
+
+
