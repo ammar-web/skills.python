be4fced8ddfb431ad62bc0e25182fdd867d0dfa1
Coding Knight <141774089+ammar-web@users.noreply.github.com>
2023-08-27 19:06:18 +0530

    new file added

---
+++ while loops.py
@@ -0,0 +1,14 @@
+secret_number=14
+
+guess_count=0
+
+guess_limit=3
+
+while guess_count < guess_limit:
+    guess=int(input("guess:"))
+    guess_count+=1
+
+if guess==secret_number:
+        print("you won")
+
+else:print("you loose")
