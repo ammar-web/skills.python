d46e3a39ec354aafc623a96321e5d7369d751dd0
Coding Knight <141774089+ammar-web@users.noreply.github.com>
2023-08-30 23:57:57 +0530

    Add files via upload

---
+++ building a car game.py
@@ -0,0 +1,30 @@
+info=input(">")
+
+
+
+if info=="help":
+    print('''Start- To start the car
+Stop- To stop a car
+Quit-To exit''')
+
+
+
+if info!="help":
+    print("I don't undestand that...")
+x=input(">")
+
+
+if x=="start":
+    print("Car started to move...")
+
+y=input(">")
+
+
+
+if y=="stop":
+    print("car stopped moving...")
+
+z=input(">")
+
+if z=="quit":
+    print("exited from game...")
