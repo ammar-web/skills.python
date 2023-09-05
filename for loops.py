45648629205f6ea53f9633187a6274787f59bd55
Coding Knight <141774089+ammar-web@users.noreply.github.com>
2023-09-05 23:44:57 +0530

    Add files via upload

---
+++ for loops.py
@@ -0,0 +1,16 @@
+prices = [10, 20, 30, 100]
+
+total = 0
+
+for price in prices:
+    total += price
+
+print(f"total_payment:{total}")
+
+total_payment = total
+
+if total_payment > 200:
+    print("Oops it's out of my budget!!")
+
+if total_payment < 200:
+    print("thank you")
